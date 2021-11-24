db85052b9cbea09239e2d340e27eb10e2c5ebb56
Lucas Holanda <lucas.c.d.holanda@gmail.com>
2021-11-24 13:33:44 -0300

    Updated to include delegations

--- ContractProcessing/Entities/Services/BillGeneratorService.cs
+++ ContractProcessing/Entities/Services/BillGeneratorService.cs
@@ -4,6 +4,11 @@ using System.Globalization;
 
 namespace ContractProcessing.Entities.Services {
     class BillGeneratorService {
+        delegate string LineReplacer(string s1, string s2);
+        delegate string Substring(int i);
+        delegate double Interest(double d, int i);
+        delegate double Fee(double d);
+        delegate string CIValue(string s, IFormatProvider p);
         public static void GenerateBill(Contract contract, int payments) {
             try {
                 string sourcePath = Path.GetFullPath("BillModel.txt");
@@ -17,15 +22,22 @@ namespace ContractProcessing.Entities.Services {
                     File.Copy(sourcePath, targetPath);
 
                     using (var fileStream = new StreamWriter(targetPath)) {
-                        string line;
+                        string line = "";
+                        LineReplacer replace = line.Replace;
+                        Substring substring1 = aux.Substring;
+                        Substring substring2 = aux2.Substring;
+                        Interest interest = pay.Interest;
+                        Fee fee = pay.PayFee;
+                        CIValue cValue = contract.Installments[0].Value.ToString;
+
                         while ((line = sourcefile.ReadLine()) != null) {
-                            line = line.Replace("x%",  aux.Substring(aux.Length - 2) + "%");
-                            line = line.Replace("w%",  aux2.Substring(aux2.Length - 2) + "%");
-                            line = line.Replace(" X ", " " + pay.Interest(contract.Value, payments).ToString("F2", CultureInfo.InvariantCulture) + " ");
-                            line = line.Replace(" W ", " " + pay.PayFee(pay.Interest(contract.Value, payments)).ToString("F2", CultureInfo.InvariantCulture) + " ");
-                            line = line.Replace(" C ", " " + value + " ");
-                            line = line.Replace(" D ", " " + contract.Installments[0].Value.ToString("F2", CultureInfo.InvariantCulture) + " ");
-                            line = line.Replace(" G ", " " + Rng() + " ");
+                            line = replace("x%", substring1(aux.Length - 2) + "%");
+                            line = replace("w%", substring2(aux2.Length - 2) + "%");
+                            line = replace(" X ", " " + interest(contract.Value, payments).ToString("F2", CultureInfo.InvariantCulture) + " ");
+                            line = replace(" W ", " " + fee(interest(contract.Value, payments)).ToString("F2", CultureInfo.InvariantCulture) + " ");
+                            line = replace(" C ", " " + value + " ");
+                            line = replace(" D ", " " + cValue("F2", CultureInfo.InvariantCulture) + " ");
+                            line = replace(" G ", " " + Rng() + " ");
                             fileStream.WriteLine(line);
                         }
                         fileStream.Close();
@@ -47,10 +59,10 @@ namespace ContractProcessing.Entities.Services {
                 } else if (i == 7) {
                     string aux = rnd.Next(0, 9999).ToString();
                     fullCode += $"{aux}";
-                } else if (i == 0 || i == 2 || i == 4){
+                } else if (i == 0 || i == 2 || i == 4) {
                     string aux = rnd.Next(0, 99999).ToString();
                     fullCode += $"{aux}.";
-                } else if(i == 8 || i== 9){
+                } else if (i == 8 || i == 9) {
                     string aux = rnd.Next(0, 99999).ToString();
                     fullCode += $"{aux}";
                 } else {
